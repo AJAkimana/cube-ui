00764a2b7777178e765e8784dd584007437a9a34
fabriceumuhire <fabrice.umuhire.ubugeni@gmail.com>
2021-03-19 15:20:54 +0200

    Chore for creation of react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Welcome to Time Capsule 3D</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
