2bfb784b474e46ba373d139cd12a00e3b22b6ec3
fabriceumuhire <fabrice.umuhire.ubugeni@gmail.com>
2021-03-24 10:04:58 +0200

    feature for authentication

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
-import './App.css';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
+import LoginPage from './pages/login.page';
+import HomePage from './pages/home.page';
+import { signout } from './actions/login.actions';
 
 function App() {
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+  const dispatch = useDispatch();
+  const signoutHandler = () => {
+    dispatch(signout());
+  };
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <h2>Welcome to Time Capsule 3D</h2>
+      <header className="App-Header">
+        <h2>ARI CUBE</h2>
+        {userInfo ? (
+              <div className="dropdown">
+                <Link to="#">
+                  {userInfo.data.user.fullName} <i className="fa fa-caret-down"></i>{' '}
+                </Link>
+                <ul className="dropdown-content">
+                  <li>
+                    <Link to="signout" onClick={signoutHandler}>
+                      Sign Out
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            ) : (
+              <Link to="/login">Login</Link>
+            )}
       </header>
+      <main>
+      <Route path="/" component={HomePage} exact />
+      <Route path="/login" component={LoginPage} />
+      </main>
     </div>
+    </BrowserRouter>
   );
 }
 
