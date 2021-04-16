6ac6c6291207dbddddda9a0341223952a7019d1e
Akimana Jean d Amour <akimanaja17@gmail.com>
2021-04-16 10:55:39 +0200

    Refactor the base files

--- src/App.js
+++ src/App.js
@@ -1,46 +1,21 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { BrowserRouter, Link, Route } from 'react-router-dom';
-import LoginPage from './pages/login.page';
-import HomePage from './pages/home.page';
-import { signout } from './actions/login.actions';
+import { Provider as StoreProvider } from 'react-redux';
+import { Router } from 'react-router-dom';
+import { store } from './redux/store';
+import { createBrowserHistory } from 'history';
+import { ToastContainer } from 'react-toastify';
+import routes from './routes';
 
 function App() {
-  const userSignin = useSelector((state) => state.userSignin);
-  const { userInfo } = userSignin;
-  const dispatch = useDispatch();
-  const signoutHandler = () => {
-    dispatch(signout());
-  };
-  return (
-    <BrowserRouter>
-    <div className="App">
-      <header className="App-Header">
-        <h2>Augmented Reality Innovations</h2>
-        {userInfo ? (
-              <div className="dropdown">
-                <Link to="#">
-                  {userInfo.data.user.fullName} <i className="fa fa-caret-down"></i>{' '}
-                </Link>
-                <ul className="dropdown-content">
-                  <li>
-                    <Link to="/" onClick={signoutHandler}>
-                      Sign Out
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-            ) : (
-              <Link to="/">Home</Link>
-            )}
-      </header>
-      <main>
-        <Route path="/" component={LoginPage} exact />
-        <Route path="/home" component={HomePage} />
-      </main>
-    </div>
-    </BrowserRouter>
-  );
+	const history = createBrowserHistory();
+	return (
+		<StoreProvider store={store}>
+			<Router history={history}>
+				<ToastContainer />
+				{renderRoutes(routes)}
+			</Router>
+		</StoreProvider>
+	);
 }
 
 export default App;
