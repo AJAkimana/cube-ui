636e5ee676ab4bd39cfd097d36cb7e78652ab05e
Akimana Jean d Amour <akimanaja17@gmail.com>
2021-05-12 10:45:43 +0200

    Add actions to customer table view, edit, and delete

--- src/components/columns/userColumns.js
+++ src/components/columns/userColumns.js
@@ -1,9 +1,59 @@
-export const userColumns = () => [
-  { path: "fullName", label: "Names" },
+import React from "react";
+import {
+  ButtonGroup,
+  IconButton,
+  Tooltip,
+  Typography,
+} from "@material-ui/core";
+import {
+  EditRounded as EditIcon,
+  ViewComfy as ViewComfyIcon,
+  DeleteForeverRounded as DeleteIcon,
+} from "@material-ui/icons";
+
+export const userColumns = (onUserClick) => [
+  {
+    content: (item) => (
+      <Typography>{`${item.firstName} ${item.lastName}`}</Typography>
+    ),
+    label: "Names",
+  },
   { path: "email", label: "Email" },
   { path: "phoneNumber", label: "Phone number" },
-  { path: "country", label: "Country" },
-  { path: "city", label: "City" },
   { path: "role", label: "User role" },
   { path: "companyName", label: "Company" },
+  {
+    content: (item) => (
+      <ButtonGroup variant="outlined">
+        <Tooltip title="View">
+          <IconButton
+            aria-label="View"
+            color="secondary"
+            onClick={() => onUserClick(item, "view")}
+          >
+            <ViewComfyIcon /> View
+          </IconButton>
+        </Tooltip>
+        <Tooltip title="Edit">
+          <IconButton
+            aria-label="Edit"
+            color="secondary"
+            onClick={() => onUserClick(item, "edit")}
+          >
+            <EditIcon />
+          </IconButton>
+        </Tooltip>
+        <Tooltip title="Delete">
+          <IconButton
+            aria-label="Delete"
+            color="danger"
+            onClick={() => onUserClick(item, "delete")}
+          >
+            <DeleteIcon />
+          </IconButton>
+        </Tooltip>
+      </ButtonGroup>
+    ),
+    label: "Actions",
+  },
 ];
