948d432fe0428dca4a1287ff39a5b56f24d8b3c6
Akimana Jean d Amour <akimanaja17@gmail.com>
2021-05-12 10:50:38 +0200

    Changes for view, edit, and delete actions

--- src/pages/Customer/Customer.js
+++ src/pages/Customer/Customer.js
@@ -2,19 +2,34 @@ import React, { useEffect, useState } from "react";
 import { Registration } from "./Registration";
 import { useSelector } from "react-redux";
 import { Box, Grid } from "@material-ui/core";
-import { getUsersList } from "redux/actions/user";
+import { deleteUser, getUsersList } from "redux/actions/user";
 import { initialPaginate, paginate } from "utils/paginate";
 import { CustomisedTable } from "components/CustomizedTable";
 import { userColumns } from "components/columns/userColumns";
+import { CustomerModel } from "./CustomerModel";
+import { AlertConfirm } from "components/AlertConfirm";
 
 export const CustomerPage = () => {
   const [paginatedUsers, setPaginatedUsers] = useState([]);
   const [paginator, setPaginator] = useState(initialPaginate(10));
-  const userList = useSelector((state) => state.userList);
-  const { loading, users } = userList;
+  const [currentItem, setCurrentItem] = useState(null);
+  const [action, setAction] = useState("add");
+  const [openView, setOpenView] = useState(false);
+  const [confirmDel, setConfirmDel] = useState(false);
+
+  const userState = useSelector((state) => state);
+  const {
+    userList: { loading, users },
+    userRm: { loading: deleting, loaded: deleted },
+  } = userState;
   useEffect(() => {
     getUsersList();
   }, []);
+  useEffect(() => {
+    if (deleted) {
+      getUsersList();
+    }
+  }, [deleted]);
   useEffect(() => {
     if (users.length > 0) {
       const { pageNumber, pageSize } = paginator;
@@ -25,6 +40,18 @@ export const CustomerPage = () => {
   const onPageChange = ({ selected }) => {
     setPaginator({ ...paginator, pageNumber: selected + 1 });
   };
+  const onUserClick = (user = {}, action) => {
+    setCurrentItem(user);
+    if (action === "delete") {
+      setConfirmDel(true);
+      return;
+    }
+    if (action === "view") {
+      setOpenView(true);
+      return;
+    }
+    setAction(action);
+  };
   return (
     <Box
       sx={{
@@ -34,14 +61,29 @@ export const CustomerPage = () => {
       }}
     >
       <Grid container spacing={1}>
+        <CustomerModel
+          open={openView}
+          setOpen={() => setOpenView(false)}
+          currentItem={currentItem}
+        />
+        <AlertConfirm
+          loading={deleting}
+          message={
+            currentItem &&
+            `Are you sure you want to delete ${currentItem.firstName}`
+          }
+          onConfirmYes={() => deleteUser(currentItem._id)}
+          open={confirmDel}
+          setOpen={() => setConfirmDel(false)}
+        />
         <Grid item lg={4} md={4} xl={6} xs={12}>
-          <Registration />
+          <Registration action={action} currentItem={currentItem} />
         </Grid>
         <Grid item lg={8} md={8} xl={6} xs={12}>
           <CustomisedTable
             tableTitle="All users"
             loading={loading}
-            columns={userColumns()}
+            columns={userColumns(onUserClick)}
             dataCount={paginatedUsers.length}
             data={paginatedUsers}
             withPagination
