9d7719bf1277d7e4a6e056733843ca0ae7b94d80
Akimana Jean d Amour <akimanaja17@gmail.com>
2021-05-21 11:48:49 +0200

    Show dates and no of items in project view

--- src/pages/Project/ProjectModal.js
+++ src/pages/Project/ProjectModal.js
@@ -9,11 +9,15 @@ import {
   DialogTitle,
   Slide,
   CardContent,
+  ListItemText,
+  ListItem,
+  Typography,
 } from "@material-ui/core";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import TextInfoContent from "@mui-treasury/components/content/textInfo";
 import { useBlogTextInfoContentStyles } from "@mui-treasury/styles/textInfoContent/blog";
+import { projectTypes } from "./projectConstants";
 
 const Transition = forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
@@ -22,6 +26,7 @@ const Transition = forwardRef(function Transition(props, ref) {
 export const ProjectModel = ({ open = false, setOpen, currentItem = null }) => {
   const [project, setProject] = useState({ name: "" });
   const [events, setEvents] = useState([]);
+  const [projectType, setProjectType] = useState({});
   useEffect(() => {
     if (currentItem) {
       setProject(currentItem);
@@ -33,6 +38,10 @@ export const ProjectModel = ({ open = false, setOpen, currentItem = null }) => {
           allDay: true,
         },
       ]);
+      const currentPType = projectTypes.find(
+        (e) => e.name === currentItem.type
+      );
+      setProjectType(currentPType);
     }
   }, [currentItem]);
   const { button: buttonStyles, ...contentStyles } =
@@ -58,10 +67,22 @@ export const ProjectModel = ({ open = false, setOpen, currentItem = null }) => {
               project.user?.firstName &&
               `Created by: ${project.user.firstName} ${project.user.lastName}`
             }
-            heading={`Project duration: ${moment(project.startDate).format(
-              "YYYY-MM-DD"
-            )} to ${moment(project.dueDate).format("YYYY-MM-DD")}`}
           />
+          <ListItemText
+            primary={`Start: ${moment(project.startDate).format(
+              "YYYY-MM-DD"
+            )}, Due date: ${moment(project.dueDate).format("YYYY-MM-DD")}`}
+          />
+          <ListItem>
+            <Typography variant="h4">Project type:</Typography>
+            <ListItemText
+              primary={projectType?.name}
+              secondary={projectType?.description}
+            />
+          </ListItem>
+          <Typography variant="h4">
+            Number of items: {project.nOfItems}
+          </Typography>
           {HtmlParser(project.description)}
           <Button className={buttonStyles}>
             Budget: ${project.budget?.toLocaleString("en-US") || 0}
--- src/pages/Project/ProjectRegistration.js
+++ src/pages/Project/ProjectRegistration.js
@@ -23,39 +23,12 @@ import { Loading } from "components/loading.component";
 import { useStyles } from "styles/formStyles";
 import { getUsersList } from "redux/actions/user";
 import { DraftEditor } from "components/DraftEditor";
+import {
+  initialState,
+  projectStatuses,
+  projectTypes,
+} from "./projectConstants";
 
-const initialState = {
-  name: "",
-  type: "",
-  nOfItems: "1",
-  startDate: moment().format("YYYY-MM-DD"),
-  dueDate: moment().format("YYYY-MM-DD"),
-  budget: "",
-  status: "pending",
-  description: "",
-  userId: "",
-  managerId: "",
-};
-const projectStatuses = ["pending", "approved", "canceled"];
-const projectTypes = [
-  {
-    name: "Cube Platform",
-    description: "I want to publish and manage my products in 3D and AR myself",
-  },
-  {
-    name: "3D modeling",
-    description: "I need help with creating my products in 3D",
-  },
-  {
-    name: "3D Viewer",
-    description: "I want to present my product in 3D on a website",
-  },
-  {
-    name: "3D Configurator",
-    description: "I want to showcase my products in specific configurations",
-  },
-  { name: "AR", description: "I want to showcase my products in AR" },
-];
 export const ProjectRegistration = ({ action = "add", currentItem = null }) => {
   const classes = useStyles();
   const [values, setValues] = useState(initialState);
