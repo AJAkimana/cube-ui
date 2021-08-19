f36770a0858f26ed1be1be6751a530ddeee36bc5
Akimana A Jean d Amour <akimanaja17@gmail.com>
2021-08-19 09:35:36 +0200

    Fix image path

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
 export const USER_INFO = "userInfo";
 export const INVOICE_ROUTE = `${process.env.REACT_APP_API_URL}api/v1/invoice`;
-export const IMAGES_PATH = `${process.env.REACT_APP_API_URL}api/v1/images`;
+export const IMAGES_PATH = `${process.env.REACT_APP_API_URL}api/images`;
