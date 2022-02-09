6800da92e3e2b75ad2c7c71ccde6ddaf53f5b298
Akimana A Jean d Amour <akimanaja17@gmail.com>
2022-02-09 15:09:55 +0200

    Add a debugger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,31 +1,34 @@
-import { applyMiddleware, createStore, compose } from 'redux';
-import thunkMiddleware from 'redux-thunk';
-import promise from 'redux-promise-middleware';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { applyMiddleware, createStore, compose } from "redux";
+import thunkMiddleware from "redux-thunk";
+import promise from "redux-promise-middleware";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-import rootReducer from './reducers';
-import { errorHandler } from './utils';
+import rootReducer from "./reducers";
+import { errorHandler } from "./utils";
 
 const initialState = {
-	login: {
-		userInfo: localStorage.getItem('userInfo')
-			? JSON.parse(localStorage.getItem('userInfo'))
-			: { user: {} }
-	}
+  login: {
+    userInfo: localStorage.getItem("userInfo")
+      ? JSON.parse(localStorage.getItem("userInfo"))
+      : { user: {} },
+  },
 };
 const configureStore = (preloadedState = initialState) => {
-	const isDev = process.env.NODE_ENV === 'development';
-	const middlewares = [errorHandler, thunkMiddleware, promise]; // loggerMiddleware
+  const isDev = process.env.NODE_ENV === "development";
+  const middlewares = [errorHandler, thunkMiddleware, promise]; // loggerMiddleware
 
-	const middlewareEnhancer = isDev
-		? composeWithDevTools(applyMiddleware(...middlewares))
-		: applyMiddleware(...middlewares);
+  // const middlewareEnhancer = isDev
+  // 	? composeWithDevTools(applyMiddleware(...middlewares))
+  // 	: applyMiddleware(...middlewares);
+  const middlewareEnhancer = composeWithDevTools(
+    applyMiddleware(...middlewares)
+  );
 
-	const enhancers = [middlewareEnhancer];
-	const composedEnhancers = compose(...enhancers);
+  const enhancers = [middlewareEnhancer];
+  const composedEnhancers = compose(...enhancers);
 
-	const store = createStore(rootReducer, preloadedState, composedEnhancers);
+  const store = createStore(rootReducer, preloadedState, composedEnhancers);
 
-	return store;
+  return store;
 };
 export const store = configureStore();
